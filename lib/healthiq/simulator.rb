2731fecf1e0876160d6a44184433607d9163c5f0
phongleland <phongleland@Phongs-iMac.local>
2016-01-13 14:43:32 -0800

    simulator

--- lib/healthiq/simulator.rb
+++ lib/healthiq/simulator.rb
@@ -1,14 +1,55 @@
 class Healthiq::Simulator
 
-  attr_reader :body_inputs
+  attr_reader :body_inputs, :bg_level, :glycation_level
   
-  def initialize
+  PRECISION = 2
+  NORMAL_LEVEL = 80
+  GLYCATION_TRIGGER = 150
+  
+  def initialize(opts={})
+    @bg_level = opts[:bg_level]
+    @glycation_level = opts[:glycation_level]
     @body_inputs = []
   end
 
   def add(input)
-   
     @body_inputs << input
+  end
+  
+  def simulate_readings(t1, t2)
+    bg_readings = []
+    bg_readings << bg_level
+    glycations = []
+    glycations << glycation_level
+    while t1 <= t2
+      bg_reading = normalize(bg_readings.last, bg_contributions_at(t1)).round(PRECISION)
+      bg_readings << bg_reading
+      glycations << glycation(glycations.last, bg_reading)
+      t1 += 60
+    end
     
+    {:bg => bg_readings, :glycation => glycations }
+  end
+  
+  def glycation(current_glycation, bg_reading)
+    if (bg_reading > GLYCATION_TRIGGER)
+      return (current_glycation + 1)
+    else
+      return current_glycation
+    end
+  end
+  
+  def normalize(basal_bg_level, contributing_bg_reading)
+    if contributing_bg_reading == 0 
+      return basal_bg_level if basal_bg_level == NORMAL_LEVEL
+      return basal_bg_level - 1 if basal_bg_level > NORMAL_LEVEL
+      return basal_bg_level + 1 if basal_bg_level < NORMAL_LEVEL
+    else
+      return (basal_bg_level + contributing_bg_reading)
+    end
+  end
+  
+  def bg_contributions_at(time)
+    return body_inputs.map{|item| item.bg_contribution_at(time)}.inject(0) {|sum, i|  sum + i }.round(PRECISION)
   end
 end
--- spec/simulator_spec.rb
+++ spec/simulator_spec.rb
@@ -2,7 +2,79 @@ require_relative 'spec_helper'
 
 describe 'Healthiq::Simulator' do
   before do
-    @simulator = Healthiq::Simulator.new
+    @simulator = Healthiq::Simulator.new({:bg_level => 80, :glycation_level => 0 })
+  end
+  
+  describe '#add' do
+    it "should add input to body_input array" do
+      food = Healthiq::Food.parse("2013-12-01T16:19:00Z\t50")
+      exercise = Healthiq::Exercise.parse("2013-12-01T16:19:00Z\t70")
+      @simulator.add(food)
+      @simulator.add(exercise)
+      expect(@simulator.body_inputs.count).to eq(2)
+    end
+  end
+  
+  describe "#bg_contributions_at" do
+    it "should sum up readings from all body_inputs" do
+      food = Healthiq::Food.parse("2013-12-01T16:19:00Z\t50")
+      exercise = Healthiq::Exercise.parse("2013-12-01T16:19:00Z\t70")
+      @simulator.add(food)
+      @simulator.add(exercise)
+      # 1 minute after
+      # 0.05 + -0.07
+      expect(@simulator.bg_contributions_at(Time.utc(2013,12,1,16,20))).to eq(-0.02)
+      # 2 minute after
+      # 0.10 + -0.14
+      expect(@simulator.bg_contributions_at(Time.utc(2013,12,1,16,21))).to eq(-0.04)
+    end
+    
+  end
+  
+  describe "#normalize" do
+    it "shoudld return sum if contributing number is greater than 0" do
+      expect(@simulator.normalize(1, 2)).to eq(3)
+    end
+    it "should return basal + 1 if contributing is 0 and basal lt 80" do
+      expect(@simulator.normalize(5, 0)).to eq(6)
+    end
+    it "should return basal - 1 if contributing is 0 and basal gt 80" do
+      expect(@simulator.normalize(88, 0)).to eq(87)
+    end
+    it "should return basal  if contributing is 0 and basal eq 80" do
+      expect(@simulator.normalize(80, 0)).to eq(80)
+    end
+  end
+  
+  describe "#glycation" do
+    it "should return current glycation if bg_level is lt 150" do
+      expect(@simulator.glycation(33, 100)).to eq(33)
+    end
+    it "should return current glycation if bg_level is eq 150" do
+      expect(@simulator.glycation(33, 150)).to eq(33)
+    end
+    it "should return current glycation + 1 if bg_level is gt 150" do
+      expect(@simulator.glycation(33, 151)).to eq(34)
+    end
+  end
+  
+  
+  describe "#simulate_readings" do
+    it "should return array of readings" do
+      food = Healthiq::Food.parse("2013-12-01T16:19:00Z\t50")
+      exercise = Healthiq::Exercise.parse("2013-12-01T16:19:00Z\t70")
+      @simulator.add(food)
+      @simulator.add(exercise)
+      # 4 hours of readings
+      readings = @simulator.simulate_readings(Time.utc(2013,12,1,16,20), Time.utc(2013,12,1,22,21))
+      p readings
+      bg_readings = readings[:bg]
+      glycation_readings = readings[:glycation]
+      expect(bg_readings[0]).to eq( 80 )
+      expect(bg_readings[1]).to eq( 79.98 )
+      expect(bg_readings[2]).to eq( 79.94 )
+
+    end
   end
   
 end
