de9a104f2c5a30cf44999057fc43f0a4a0073091
phongleland <phongleland@Phongs-iMac.local>
2016-01-13 11:02:45 -0800

    food model

--- lib/healthiq/food.rb
+++ lib/healthiq/food.rb
@@ -1,3 +1,35 @@
+require 'time'
+
 class Healthiq::Food
+  
+  attr_reader :glycemic_index, :time_ate
+  
+  MAX_MINUTE = 120
+  COEFFICIENT = 0.001
+  PRECISION = 2
+  
+  def self.parse(line)
+    parts = line.split(/\t/)
+    self.new(
+      :time_ate => Time.parse(parts[0]),
+      :glycemic_index => parts[1].to_i
+    )
+  end
+  
+  def initialize(opts={})
+    @time_ate = opts[:time_ate]
+    @glycemic_index = opts[:glycemic_index].to_i
+  end
+  
+  def glycemic_index
+    @glycemic_index ||= 0
+  end
+  
+  def bg_contribution_after_minute(min)
+    min = min.to_i
+    return 0 if min <= 0
+    return 0 if min > MAX_MINUTE
+    return (glycemic_index * min * COEFFICIENT).round(PRECISION)
+  end
   
 end
--- spec/food_spec.rb
+++ spec/food_spec.rb
@@ -2,4 +2,38 @@ require_relative 'spec_helper'
 
 describe "Healthiq::Food" do
   
+  describe "#parse" do
+
+    it "should parse correctly" do
+      food = Healthiq::Food.parse("2013-12-01T16:19:00Z\t47")
+      expect(food.time_ate).to eq(Time.utc(2013,12,1,16,19))
+      expect(food.glycemic_index).to eq(47)
+    end
+
+  end
+  
+  describe "#bg_contribution_after_minute" do
+    
+    it "should return 0 if minute param is zero" do
+      food = Healthiq::Food.parse("2013-12-01T16:19:00Z\t47")
+      expect(food.bg_contribution_after_minute(0)).to eq(0)
+    end
+    
+    it "should return 0 if minute param is negative" do
+      food = Healthiq::Food.parse("2013-12-01T16:19:00Z\t47")
+      expect(food.bg_contribution_after_minute(-1)).to eq(0)
+    end
+    
+    it "should return 0 if minute param is gt 120" do
+      food = Healthiq::Food.parse("2013-12-01T16:19:00Z\t47")
+      expect(food.bg_contribution_after_minute(121)).to eq(0)
+    end
+    
+    it "should return a number if minute param is gt than 0 and lte 120" do
+      food = Healthiq::Food.parse("2013-12-01T16:19:00Z\t47")
+      expect(food.bg_contribution_after_minute(10)).to eq(0.47)
+    end
+    
+  end
+  
 end
